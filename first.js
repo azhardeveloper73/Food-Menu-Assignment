c09cb8617e1775e94f41f5395c89c84c9c50ef35
Azhar Vayani <azhardeveloper73@gmail.com>
2026-01-01 20:19:19 +0500

    Add Food-Menu-Assignment files

---
+++ first.js
@@ -0,0 +1,210 @@
+const resturantMenu = [
+    {
+        title: "Halwa Puri",
+        price: 100,
+        description: "Mazedar Halwa Puri",
+        imgUrl: "https://tse1.mm.bing.net/th/id/OIP.EYniJDIj1I9FJtXWY0zsbAHaEc?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "breakFast"
+
+    },
+        {
+        title: "Aallo Pratha",
+        price: 250,
+        description: "Mazedar Aallo Pratha",
+        imgUrl: "https://i.ytimg.com/vi/MH083m_9lyM/maxresdefault.jpg",
+        category: "breakFast"
+
+    },
+        {
+        title: "Bread Makhan",
+        price: 150,
+        description: "Mazedar Bread Makhan",
+        imgUrl: "https://i.ytimg.com/vi/vRvsCIEdGTs/maxres2.jpg?sqp=-oaymwEoCIAKENAF8quKqQMcGADwAQH4Ac4FgAKACooCDAgAEAEYXyBlKFowDw==&rs=AOn4CLAxL8jtjyA55StynmOjAf3WI2zwXg",
+        category: "breakFast"
+
+    },
+        {
+        title: "Chana Pratha",
+        price: 200,
+        description: "Mazedar Chana Pratha",
+        imgUrl: "https://tse2.mm.bing.net/th/id/OIP.EChfpcBZBZi5RRwwkPepdAHaFj?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "breakFast"
+
+    },
+        {
+        title: "Biscuites",
+        price: 50,
+        description: "Mazedar Biscuites",
+        imgUrl: "https://th.bing.com/th/id/R.1d756b737e4cc45976a63b24445924db?rik=Xqh7Iy1bphmWBA&pid=ImgRaw&r=0",
+        category: "breakFast"
+
+    },
+        {
+        title: "Biryani",
+        price: 300,
+        description: "Mazedar Biryani",
+        imgUrl: "https://tse2.mm.bing.net/th/id/OIP.XkvqHxdfzGlybeypjHKf-gHaGN?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "lunch"
+
+    },
+        {
+        title: "Nihari",
+        price: 400,
+        description: "Mazedar Nihari",
+        imgUrl: "https://wallpapers.com/images/hd/nihari-2o83akrzabc7ccr9.jpg",
+        category: "lunch"
+
+    },
+        {
+        title: "Pullao",
+        price: 300,
+        description: "Mazedar Pullao",
+        imgUrl: "https://tse1.mm.bing.net/th/id/OIP.TUub6we0SHBS40x-BYsu2wHaEK?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "lunch"
+
+    },
+    {
+        title: "Dall Chawal",
+        price: 200,
+        description: "Mazedar Dall Chawal",
+        imgUrl: "https://tse3.mm.bing.net/th/id/OIP.inIc8PmyUmF5TseyNc7bhwHaFc?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "lunch"
+
+    },
+        {
+        title: "Paya",
+        price: 450,
+        description: "Mazedar Paya",
+        imgUrl: "https://th.bing.com/th/id/R.557b8ebd4e4defe12450987c8d2256a0?rik=rmZfij%2fZjQT0AQ&pid=ImgRaw&r=0",
+        category: "lunch"
+
+    },
+    {
+        title: "Kabab Pratha",
+        price: 350,
+        description: "Mazedar Kabab Pratha",
+        imgUrl: "https://tse3.mm.bing.net/th/id/OIP.dY22dv_H90AmzrJWkKR9AgHaEK?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "diner"
+
+    },
+        {
+        title: "Karahi",
+        price: 900,
+        description: "Mazedar Karahi",
+        imgUrl: "https://th.bing.com/th/id/R.7bedc55aee1c4ea41738d9cd42e4cb22?rik=mELm%2felSLENK8A&riu=http%3a%2f%2ftop10inpakistan.com%2fwp-content%2fuploads%2f2019%2f03%2fChicken-Karahi-Pakistani-Dishes.jpg&ehk=6AUE8KgDTOQXOYpum8Uao9Maj5XSUAzDMWFv3Esm8pg%3d&risl=&pid=ImgRaw&r=0",
+        category: "diner"
+
+    },
+        {
+        title: "Broast",
+        price: 600,
+        description: "Mazedar Broast",
+        imgUrl: "https://tse4.mm.bing.net/th/id/OIP.0GGvDoICrorMpmF7ZXb4VQHaFj?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "diner"
+
+    },
+        {
+        title: "Bar B.Q",
+        price: 800,
+        description: "Mazedar Bar B.Q",
+        imgUrl: "https://www.shutterstock.com/image-photo/barbecuebarbqbarbeque-grill-street-food-thailandmarket-600nw-585595169.jpg",
+        category: "diner"
+
+    },
+        {
+        title: "Fish",
+        price: 900,
+        description: "Mazedar Fish",
+        imgUrl: "https://tse4.mm.bing.net/th/id/OIP.62aYBCXRk1QO8cCNxBaQpgHaE7?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "diner"
+
+    },
+        {
+        title: "Water",
+        price: 50,
+        description: "Mazedar Water",
+        imgUrl: "https://tse4.mm.bing.net/th/id/OIP.spOq1zRuqQZ1_BuhXDG10AHaHa?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "drinks"
+
+    },
+        {
+        title: "Cold drinks",
+        price: 100,
+        description: "Mazedar Cold drinks",
+        imgUrl: "https://images.saymedia-content.com/.image/t_share/MTkxODQ5MTA0ODMyNDcyNTYy/dark-reality-of-cold-drinks.jpg",
+        category: "drinks"
+
+    },
+        {
+        title: "Chocolate Shake",
+        price: 300,
+        description: "Mazedar Chocolate Shake",
+        imgUrl: "https://joechocolates.com/wp-content/uploads/2024/06/heavenly-chocolate-shake.jpg",
+        category: "drinks"
+
+    },
+    {
+        title: "Rooh Afzah",
+        price: 150,
+        description: "Mazedar Rooh Afzah",
+        imgUrl: "https://tse1.explicit.bing.net/th/id/OIP.9VNjD3pjftsyLNw50yFDiwHaFj?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "drinks"
+
+    },
+        {
+        title: "Juice",
+        price: 150,
+        description: "Mazedar Juice",
+        imgUrl: "https://tse1.mm.bing.net/th/id/OIP.V6wO4PpRi-wLtGnNCujN5gHaHa?cb=ucfimg2&ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3",
+        category: "drinks"
+
+    },
+]
+
+
+
+menu()
+
+
+function menu(category) {
+    const myMenuHTMLForBody = resturantMenu.filter(function(menu) {
+        if (category) {
+            return menu.category.toLowerCase() == category.toLowerCase()
+        } else {
+            return true
+        }
+        
+    }).map(function (menu) {
+    const myMenuHTML = `<div class="foodItem">
+            <!-- image -->
+            <div class="img">
+                <img src="${menu.imgUrl}"
+                    alt="">
+            </div>
+            <div class="titleDescription">
+                <!-- tittle & price -->
+                <div class="titlePrice">
+                    <h3>${menu.title}</h3>
+                    <p>${menu.price}</p>
+                </div>
+                <hr>
+                <!-- description -->
+                <p>${menu.description}</p>
+            </div>
+        </div>`
+
+        return myMenuHTML
+})    
+
+document.querySelector("#foodItems").innerHTML = myMenuHTMLForBody.join("")
+
+}
+
+const buttons = document.querySelectorAll(".btns button");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    buttons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
